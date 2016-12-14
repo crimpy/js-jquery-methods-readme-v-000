e37a5f5590dd9274d3281e8be0f57495c468e139
Brianna Titone <briannactitone@gmail.com>
2016-12-14 03:57:40 +0000

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function fancySchmancy(){
+  $('#bacon').addClass('iAmFancy');
+  return $('#bacon').html();
+}
+
+function addText(string){
+  $('p:nth-child(2)').append(string);
+  return $('p:nth-child(2)').html();
+}
+
+function showDolphin(){
+  $("#hidden").show();
+}
+
+function hideImage(){
+  $('img[alt="pie in face"]').hide();
+}
+function fadeIt(){
+  $('#favorite-snacks').fadeIn(100);
+}
+
+function fadeItOut(){
+  return $('p:nth-child(1)').fadeOut(0);
+}
+
+function findItByClass(type){
+  return $(type).hasClass('flatironLink');
+}
+
+function formValue(){
+  return $('input').last().val();
+
+}
